6f69b7d11d690091dc40fdf93e2e055d284907ed
Adam Fisher <amgfisher90@gmail.com>
2023-02-01 12:18:59 -0500

    commit tree b4efb13d85aecbf71a621080bcf047f27e0092fa

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,20 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+    
+    if params[:name]
+
+      search_name = params[:name].downcase
+      students = students.filter do |student|
+        student.first_name.downcase == search_name|| student.last_name.downcase == search_name
+      end
+    end
+
     render json: students
+  end
+
+  def show
+   render json: Student.find(params[:id])
   end
 
 end
